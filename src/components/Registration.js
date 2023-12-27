c39bcdc4a212d43a69106950e7adeff348f0e936
Haithem Albetairi <haithem.t.albetairi@gmail.com>
2023-12-27 04:42:36 +0300

    Updated Components
    Registration and login

--- src/components/Registration.js
+++ src/components/Registration.js
@@ -1,20 +1,6 @@
 // Registration.js
 import React, { useState } from 'react';
 
-import firebase from 'firebase/app';
-import 'firebase/auth';
-const firebaseConfig = {
-  apiKey: "AIzaSyA0nP2NYmqe-3ZCurR74QL_JgWklFPOIVw",
-  authDomain: "job-posting-409011.firebaseapp.com",
-  projectId: "job-posting-409011",
-  storageBucket: "job-posting-409011.appspot.com",
-  messagingSenderId: "848172609833",
-  appId: "1:848172609833:web:d50da1a1cd7e97346c8738",
-  measurementId: "G-G7YJ0X7PRN"
-};
-
-firebase.initializeApp(firebaseConfig);
-
 const Registration = ({ onLogin }) => {
   const [formData, setFormData] = useState({
     email: '',
@@ -32,15 +18,6 @@ const Registration = ({ onLogin }) => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    const register = async (email, password) => {
-      try {
-        const userCredential = await firebase.auth().createUserWithEmailAndPassword(email, password);
-        return userCredential.user;
-      } catch (error) {
-        console.error("Registration error:", error);
-        throw error;
-      }
-    };
     console.log('Registration data submitted:', formData);
     onLogin();
   };
@@ -54,7 +31,7 @@ const Registration = ({ onLogin }) => {
             type="text"
             id="email"
             name="email"
-            placeholder="Password"
+            placeholder="Email"
             value={formData.email}
             onChange={handleChange}
             required
@@ -72,29 +49,27 @@ const Registration = ({ onLogin }) => {
           />
         </div>
         <div className="form-group">
-          <label>User Type:</label>
-          <div className='form-group'>
-            <input
-              type="radio"
-              name="userType"
-              value="job_hunter"
-              checked={formData.userType === 'job_hunter'}
-              onChange={handleChange}
-            />
+          <label><strong>User Type:</strong></label>
             <label>
               Job Hunter
+              <input
+                type="checkbox"
+                name="userType"
+                value="job_hunter"
+                checked={formData.userType === 'job_hunter'}
+                onChange={handleChange}
+              />
             </label>
-            <input
-              type="radio"
-              name="userType"
-              value="recruiter"
-              checked={formData.userType === 'recruiter'}
-              onChange={handleChange}
-            />
             <label>
               Recruiter
+              <input
+                type="checkbox"
+                name="userType"
+                value="recruiter"
+                checked={formData.userType === 'recruiter'}
+                onChange={handleChange}
+              />
             </label>
-          </div>
         </div>
         <button type="submit" className='apply-button'>Register</button>
       </form>
